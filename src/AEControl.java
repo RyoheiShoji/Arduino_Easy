023a3212f164812b94cf51d3d26d261a56f9b43c
Ryohei Shoji <ryohei.shoji.sys@gmail.com>
2017-10-24 10:45:46 +0900

    first

---
+++ src/AEControl.java
@@ -0,0 +1,32 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class AEControl extends JPanel implements KeyListener,Runnable{
+
+	@Override
+	public void run() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/Arduino_Easy.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Arduino_Easy extends JFrame{
+
+	public Arduino_Easy(){
+
+	}
+
+	public static void main(String[] args){
+		JFrame f = new Arduino_Easy();
+		f.setVisible(true);
+	}
+}
+
+
