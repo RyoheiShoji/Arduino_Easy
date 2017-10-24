07d93e0604fed1752734d03414c10af6ca9dacd3
Ryohei Shoji <ryohei.shoji.sys@gmail.com>
2017-10-24 10:54:20 +0900

    second

--- src/AEControl.java
+++ src/AEControl.java
@@ -7,26 +7,18 @@ public class AEControl extends JPanel implements KeyListener,Runnable{
 
 	@Override
 	public void run() {
-
-
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
-
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
-
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
-
 	}
 
 }
--- src/Arduino_Easy.java
+++ src/Arduino_Easy.java
@@ -1,9 +1,24 @@
+import java.awt.Color;
+import java.awt.Container;
+
 import javax.swing.JFrame;
 
 public class Arduino_Easy extends JFrame{
-
+	Container c;
+	static AEControl ae;
 	public Arduino_Easy(){
+		//一番外側のコンテナの定義
+		setTitle("シューティングゲーム");
+		setSize(700,460);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		c = getContentPane();
+		c.setLayout(null);
 
+		//Arduino_Easyをコントロールするクラスの追加
+		ae = new AEControl();
+		ae.setBounds(0,0,660,400);
+		ae.setBackground(Color.red);
+		c.add(ae);
 	}
 
 	public static void main(String[] args){
